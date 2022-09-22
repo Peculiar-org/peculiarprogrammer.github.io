20248ab9fbd4db92fb7672b44dadf634ea6f515e
marcuwynu23 <marcuwynu23@gmail.com>
2022-09-23 00:18:48 +0800

    Initial Commit

---
+++ src/containers.js
@@ -0,0 +1,15 @@
+$("#container-list")
+
+	.append(createContainer(
+		`
+		<img src='https://img.shields.io/badge/-blue?style=flat&logo=Docker&logoColor=white'></img>
+		Peculiar alpine Docker Container
+		`,
+		{
+			name: "@marcuwynu23",
+			link: "https://github.com/marcuwynu23"
+		},
+		"Peculiar alpine Docker Container",
+		"https://hub.docker.com/repository/docker/marcuwynu23/peculiar-alpine"
+	))
+
---
+++ src/projects.js
@@ -0,0 +1,223 @@
+$("#project-list")
+	.append(createProject(
+		"Just Utility",
+		{
+			name: "@marcuwynu23",
+			link: "https://github.com/marcuwynu23"
+		},
+		"Commandline Utility for Windows",
+		[
+			"https://github.com/marcuwynu23/just-utility/blob/main/docs/images/1.jpg?raw=true",
+			"https://github.com/marcuwynu23/just-utility/blob/main/docs/images/2.jpg?raw=true",
+			"https://github.com/marcuwynu23/just-utility/blob/main/docs/images/3.jpg?raw=true"
+		],
+		"https://github.com/marcuwynu23/just-utility.git",
+		"https://github.com/marcuwynu23/just-utility/blob/main/README.md",
+		"https://github.com/marcuwynu23/just-utility/tags"
+	))
+
+	.append(createProject(
+		"Yano Text Editor",
+		{
+			name: "@marcuwynu23",
+			link: "https://github.com/marcuwynu23"
+		},
+		"Text Editor for learning programming basic or doing fast experiment programming",
+		[
+			"https://github.com/marcuwynu23/yano-text-editor/blob/main/docs/images/1.jpg?raw=true",
+			"https://github.com/marcuwynu23/yano-text-editor/blob/main/docs/images/2.jpg?raw=true",
+			"https://github.com/marcuwynu23/yano-text-editor/blob/main/docs/images/3.jpg?raw=true"
+		],
+		"https://github.com/marcuwynu23/yano-text-editor.git",
+		"https://github.com/marcuwynu23/yano-text-editor.git",
+		"https://github.com/marcuwynu23/yano-text-editor.git"
+	))
+	.append(createProject(
+		"Auto",
+		{
+			name: "@marcuwynu23",
+			link: "https://github.com/marcuwynu23"
+		},
+		"Windows Commandline tool for automating execution of commands in Windows Terminal",
+		[
+			"https://github.com/marcuwynu23/Auto/blob/main/docs/images/1.jpg?raw=true",
+			"https://github.com/marcuwynu23/Auto/blob/main/docs/images/2.jpg?raw=true"
+		],
+		"https://github.com/marcuwynu23/Auto.git",
+		"https://github.com/marcuwynu23/Auto/blob/main/README.md",
+		"https://github.com/marcuwynu23/Auto/tags"
+	))
+
+	.append(createProject(
+		"Ngita",
+		{
+			name: "@marcuwynu23",
+			link: "https://github.com/marcuwynu23"
+		},
+		`GUI Tools for easy searching queries in different searching engines and social media sites`,
+		[
+			"https://github.com/marcuwynu23/ngita/blob/main/images/1.jpeg?raw=true",
+			"https://github.com/marcuwynu23/ngita/blob/main/images/1.jpeg?raw=true"
+		],
+		"https://github.com/marcuwynu23/ngita.git",
+		"https://github.com/marcuwynu23/ngita/blob/main/README.md",
+		"https://github.com/marcuwynu23/ngita/tags"
+	))
+
+	.append(createProject(
+		"sdir",
+		{
+			name: "@marcuwynu23",
+			link: "https://github.com/marcuwynu23"
+		},
+		"Commandline tool to save current directory and show list of save directory for future use.",
+		[
+			"https://github.com/marcuwynu23/sdir/blob/main/docs/images/1.jpg?raw=true",
+			"https://github.com/marcuwynu23/sdir/blob/main/docs/images/1.jpg?raw=true"
+		],
+		"https://github.com/marcuwynu23/sdir.git",
+		"https://github.com/marcuwynu23/sdir/blob/main/README.md",
+		"https://github.com/marcuwynu23/sdir/tags"
+	))
+	.append(createProject(
+		"Tool Maker",
+		{
+			name: "@marcuwynu23",
+			link: "https://github.com/marcuwynu23"
+		},
+		"C/C++ Simple Dynamic library project for commandline tool creation and shell automation",
+		[
+			"https://github.com/marcuwynu23/toolmaker/blob/main/docs/images/1.jpg?raw=true",
+			"https://github.com/marcuwynu23/toolmaker/blob/main/docs/images/1.jpg?raw=true",
+			"https://github.com/marcuwynu23/toolmaker/blob/main/docs/images/1.jpg?raw=true"
+		],
+		"https://github.com/marcuwynu23/toolmaker.git",
+		"https://github.com/marcuwynu23/toolmaker/blob/main/README.md",
+		"https://github.com/marcuwynu23/toolmaker/tags"
+	))
+
+	.append(createProject(
+		"dyna",
+		{
+			name: "@marcuwynu23",
+			link: "https://github.com/marcuwynu23"
+		},
+		"Commandline Tool for creating NodeJS Web Application Project",
+		[
+			"https://github.com/marcuwynu23/dyna/blob/main/docs/images/1.jpg?raw=true",
+			"https://github.com/marcuwynu23/dyna/blob/main/docs/images/1.jpg?raw=true",
+			"https://github.com/marcuwynu23/dyna/blob/main/docs/images/1.jpg?raw=true"
+		],
+		"https://github.com/marcuwynu23/dyna.git",
+		"https://github.com/marcuwynu23/dyna/blob/main/README.md",
+		"https://github.com/marcuwynu23/dyna/tags"
+	))
+
+
+	.append(createProject(
+		"JustLib",
+		{
+			name: "@marcuwynu23",
+			link: "https://github.com/marcuwynu23"
+		},
+		`Javascript Library for Web Application Development`,
+		[],
+		"https://github.com/marcuwynu23/justlib.git",
+		"https://github.com/marcuwynu23/justlib/blob/main/README.md",
+		"https://github.com/marcuwynu23/justlib/tags"
+
+	))
+	.append(createProject(
+		"Object File Encrypter",
+		{
+			name: "@marcuwynu23",
+			link: "https://github.com/marcuwynu23"
+		},
+		"	Node Library to convert/retrieve encrypted object to/from file",
+		[],
+		"https://github.com/marcuwynu23/object-file-encrypter.git",
+		"https://github.com/marcuwynu23/object-file-encrypter/blob/main/README.md",
+		"https://www.npmjs.com/package/object-file-encrypter"
+	))
+
+	.append(createProject(
+		"QR Code Generator",
+		"@marcuwynu23",
+		"Sample QR Code Generator Web Application",
+		[],
+		"https://github.com/marcuwynu23/qr-code-generator.git",
+		"https://github.com/marcuwynu23/qr-code-generator/blob/main/README.md",
+		"https://github.com/marcuwynu23/qr-code-generator/tags"
+	))
+
+
+	.append(createProject(
+		"WPort",
+		{
+			name: "@marcuwynu23",
+			link: "https://github.com/marcuwynu23"
+		},
+		"Commandline Tool to check Port Information",
+		[],
+		"https://github.com/marcuwynu23/what-port-is.git",
+		"https://github.com/marcuwynu23/what-port-is/blob/main/README.md",
+		"https://github.com/marcuwynu23/what-port-is/tags"
+	))
+
+
+	.append(createProject(
+		"DroidDroid",
+		{
+			name: "@marcuwynu23",
+			link: "https://github.com/marcuwynu23"
+		},
+		"[Deprecated] Android Development Tool/Helper for android development in android device and windows pc",
+		[],
+		"https://github.com/marcuwynu23/droid-droid.git",
+		"https://github.com/marcuwynu23/droid-droid/blob/main/README.md",
+		"https://github.com/marcuwynu23/droid-droid/tags"
+	))
+
+
+
+	.append(createProject(
+		"Peculiar GUI",
+		{
+			name: "@marcuwynu23",
+			link: "https://github.com/marcuwynu23"
+		},
+		"[Deprecated] Java GUI Library for rapid application development",
+		[],
+		"https://github.com/marcuwynu23/peculiar-gui.git",
+		"https://github.com/marcuwynu23/peculiar-gui/blob/main/README.md",
+		"https://github.com/marcuwynu23/peculiar-gui/tags"
+	))
+
+	.append(createProject(
+		"justutil-jcinf",
+		{
+			name: "@marcuwynu23",
+			link: "https://github.com/marcuwynu23"
+		},
+		"Commandline Tool use to get java Class fields, methods and other class information",
+		[],
+		"https://github.com/marcuwynu23/justutil-jcinf.git",
+		"https://github.com/marcuwynu23/justutil-jcinf/blob/main/README.md",
+		"https://github.com/marcuwynu23/justutil-jcinf/tags"
+	))
+
+
+	.append(createProject(
+		"Code Gremoire",
+		{
+			name: "@marcuwynu23",
+			link: "https://github.com/marcuwynu23"
+		},
+		"[Deprecated] Local Text/Note Storage Application for minimalist Programmer",
+		[],
+		"https://github.com/marcuwynu23/code-grimoire.git",
+		"https://github.com/marcuwynu23/code-grimoire/blob/main/README.md",
+		"https://github.com/marcuwynu23/code-grimoire/tags"
+	))
+
+
